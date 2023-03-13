ef3f1f1f95f85c1baf06c694c7090023a495e409
Sidelamp <gilfmuxa@gmail.com>
2023-03-13 23:05:08 +0400

    Добавьте файлы проекта.

---
+++ Enum.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+enum WeaponType { Knife, Pistol, Ak47 };
+
+void PrintWeapon(WeaponType weapon) {
+	switch (weapon)
+	{
+	case Knife:
+		cout << "Knife" << endl;
+		break;
+	case Pistol:
+		cout << "Pistol" << endl;
+		break;
+	case Ak47:
+		cout << "Ak-47" << endl;
+		break;
+	default:
+		cout << "Wrong!" << endl;
+		break;
+	}
+}
+int main()
+{
+	WeaponType weapon{};
+	
+	
+	cout << "Enter the weapon number" << endl;
+	int tmpWeapon;
+	
+	cin >> tmpWeapon;
+	weapon = (WeaponType)tmpWeapon;
+
+	PrintWeapon(weapon);
+
+	return 0;
+}
